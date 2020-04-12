f77c6a26e60b1d6e190c9d43edfb8bbf1e7faab5
CHRY3D <37815739+CHRY3D@users.noreply.github.com>
2020-04-12 14:22:21 +0200

    First commit

---
+++ Assets/Editor/assignAssetbundlesEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(assignAssetbundles))]
+public class assignAssetbundlesEditor : Editor {
+    private GameObject[] prefabs;
+    private string dest;
+    private string targ;
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        assignAssetbundles vars = (assignAssetbundles)target;
+        if (GUILayout.Button("Fetch Prefabs [Assets/Resources/Prefabs]")) {
+            getPrefabs();
+        }
+        if (GUILayout.Button("Build Prefabs [Assets/Resources/$Destination]")) {
+            createDirectory();
+        }
+        
+        void getPrefabs() {
+            vars.prefabs = Resources.LoadAll<GameObject>("Prefabs");
+        }
+
+        void createDirectory() {
+            dest = vars.destination;
+            if (string.IsNullOrEmpty(dest)) {
+                dest = "Output";
+                AssetDatabase.CreateFolder("Assets/Resources", dest);
+                assignAssetbundleNames();
+            } else {
+                AssetDatabase.CreateFolder("Assets/Resources", dest);
+                assignAssetbundleNames();
+            }
+        }
+
+        void assignAssetbundleNames() {
+            foreach(GameObject prefab in vars.prefabs) {
+                GameObject temp = Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity);
+                temp.name = prefab.name;
+                PrefabUtility.SaveAsPrefabAssetAndConnect(temp, "Assets/Resources/" + dest + "/" + temp.gameObject.name + ".prefab", InteractionMode.UserAction);
+                AssetImporter.GetAtPath("Assets/Resources/" + dest + "/" + temp.gameObject.name + ".prefab").SetAssetBundleNameAndVariant(temp.gameObject.name, "");
+                DestroyImmediate(temp);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/assignAssetbundles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignAssetbundles : MonoBehaviour {
+    [Header("")]
+    [Header("Set a destination (folder name) and hit [Build Prefabs]")]
+    [Header("Usage: Put prefabs in Assets/Resources/Prefabs, hit [Fetch Prefabs]")]
+    [Header("")]
+    [Header("Generates prefabs with assetbundle names from gameobject.name")]
+    [Header("This utility can overwrite files, use with caution!")]
+
+    public string destination;
+    public GameObject[] prefabs;
+
+}
